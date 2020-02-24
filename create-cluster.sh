d30cd3a00db9eb8914dd03e86413da59be213bba
Michael Parker <michael@parker.gg>
2020-02-24 14:27:28 -0600

    improve cluster creation script

--- create-cluster.sh
+++ create-cluster.sh
@@ -4,22 +4,28 @@ set -euo errexit
 
 clusterName="kind"
 registryName="kind-registry"
-registryPort="32000"
 registryRunning="$(docker inspect -f '{{.State.Running}}' "${registryName}" 2>/dev/null || true)"
 
 if [[ "${registryRunning}" != 'true' ]]; then
-  docker run \
-    -d --restart=always -p "${registryPort}:5000" --name "${registryName}" \
+  docker run -d \
+    -p 5000:5000 \
+    --restart=always \
+    --name=${registryName} \
+    -v $HOME/registry-2:/var/lib/registry \
+    --env REGISTRY_STORAGE_DELETE_ENABLED=true \
     registry:2
 fi
 
-registryIp="$(docker inspect -f '{{.NetworkSettings.IPAddress}}' "${registryName}")"
+registryIp="$(docker network inspect bridge --format '{{(index .IPAM.Config 0).Gateway}}')"
 
-cat <<EOF | kind create cluster --name "${KIND_CLUSTER_NAME}" --config=-
+cat <<EOF | kind create cluster \
+    --name "${clusterName}" \
+    --image kindest/node:v1.15.6@sha256:18c4ab6b61c991c249d29df778e651f443ac4bcd4e6bdd37e0c83c0d33eaae78 \
+    --config=-
 kind: Cluster
 apiVersion: kind.x-k8s.io/v1alpha4
 containerdConfigPatches:
 - |-
-  [plugins."io.containerd.grpc.v1.cri".registry.mirrors."localhost:${registryPort}"]
-    endpoint = ["http://${registryIp}:${registryPort}"]
+  [plugins."io.containerd.grpc.v1.cri".registry.mirrors."localhost:5000"]
+    endpoint = ["http://${registryIp}:5000"]
 EOF
