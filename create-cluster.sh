e5ebd0567b5939d7044fe7e1458c565a226beec5
Michael Parker <michael@parker.gg>
2020-02-24 14:31:40 -0600

    wait for kind control-plane

--- create-cluster.sh
+++ create-cluster.sh
@@ -21,6 +21,7 @@ registryIp="$(docker network inspect bridge --format '{{(index .IPAM.Config 0).G
 cat <<EOF | kind create cluster \
     --name "${clusterName}" \
     --image kindest/node:v1.15.6@sha256:18c4ab6b61c991c249d29df778e651f443ac4bcd4e6bdd37e0c83c0d33eaae78 \
+    --wait 5m \
     --config=-
 kind: Cluster
 apiVersion: kind.x-k8s.io/v1alpha4
