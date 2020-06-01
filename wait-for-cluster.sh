3e79523f928463c7f93250ac220788c500fc0946
Michael Parker <michael@parker.gg>
2020-06-01 14:31:29 -0500

    misc fixes

--- wait-for-cluster.sh
+++ wait-for-cluster.sh
@@ -4,6 +4,7 @@ printf "Waiting for Kind node to be ready"
 while : ; do
     nodeReadyStatus=$(kubectl get node kind-control-plane -o jsonpath="{.status.conditions[?(@.type==\"Ready\")].status}" 2>/dev/null)
     if [[ "${nodeReadyStatus}" == "True" ]]; then
+        echo
         break
     fi
     printf '.'
@@ -14,8 +15,9 @@ printf "Waiting for CoreDNS pods to be ready"
 while : ; do
     coreDnsReadyPods=$(kubectl get deploy -n kube-system coredns -o jsonpath="{.status.readyReplicas}" 2>/dev/null)
     coreDnsExpectedReadyPods=$(kubectl get deploy -n kube-system coredns -o jsonpath="{.status.replicas}" 2>/dev/null)
-    coreDnsReady=$(expr ${coreDnsReadyPods} / ${coreDnsExpectedReadyPods})
+    coreDnsReady=$(expr ${coreDnsReadyPods} / ${coreDnsExpectedReadyPods} 2>/dev/null)
     if [[ "${coreDnsReady}" == "1" ]]; then
+        echo
         break
     fi
     printf '.'
@@ -26,6 +28,7 @@ printf "Waiting for control plane to be ready"
 while : ; do
     status=$(kubectl get pod -n kube-system kube-apiserver-kind-control-plane -o jsonpath="{.status.phase}" 2>/dev/null)
     if [[ "${status}" == "Running" ]]; then
+        echo
         break
     fi
     printf '.'
