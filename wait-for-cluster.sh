504c7c77b9a49d1686a52c157f58b0ff99fb5f34
Michael Parker <michael@parker.gg>
2020-06-01 15:24:53 -0500

    update cluster ready script to provide progress on all cluster components (#18)

--- wait-for-cluster.sh
+++ wait-for-cluster.sh
@@ -1,10 +1,58 @@
 #!/usr/bin/env bash
 
+printf "Waiting for CNI to be ready"
+while : ; do
+    cniReady=$(kubectl get daemonset -n kube-system kindnet -o jsonpath="{.status.numberReady}" 2>/dev/null)
+    if [[ "${cniReady}" == "1" ]]; then
+        echo
+        break
+    fi
+    printf '.'
+    sleep 2
+done
+
+printf "Waiting for kube-proxy to be ready"
+while : ; do
+    kubeProxyReady=$(kubectl get daemonset -n kube-system kube-proxy -o jsonpath="{.status.numberReady}" 2>/dev/null)
+    if [[ "${kubeProxyReady}" == "1" ]]; then
+        echo
+        break
+    fi
+    printf '.'
+    sleep 2
+done
+
+printf "Waiting for control plane to be ready"
 while : ; do
     status=$(kubectl get pod -n kube-system kube-apiserver-kind-control-plane -o jsonpath="{.status.phase}" 2>/dev/null)
     if [[ "${status}" == "Running" ]]; then
+        echo
         break
     fi
-    sleep 15
-    echo "Waiting for cluster to finish setting up..."
+    printf '.'
+    sleep 2
+done
+
+printf "Waiting for CoreDNS pods to be ready"
+while : ; do
+    coreDnsReadyPods=$(kubectl get deploy -n kube-system coredns -o jsonpath="{.status.readyReplicas}" 2>/dev/null)
+    coreDnsExpectedReadyPods=$(kubectl get deploy -n kube-system coredns -o jsonpath="{.status.replicas}" 2>/dev/null)
+    coreDnsReady=$(expr ${coreDnsReadyPods} / ${coreDnsExpectedReadyPods} 2>/dev/null)
+    if [[ "${coreDnsReady}" == "1" ]]; then
+        echo
+        break
+    fi
+    printf '.'
+    sleep 2
+done
+
+printf "Waiting for Kubernetes cluster to be ready"
+while : ; do
+    nodeReadyStatus=$(kubectl get node kind-control-plane -o jsonpath="{.status.conditions[?(@.type==\"Ready\")].status}" 2>/dev/null)
+    if [[ "${nodeReadyStatus}" == "True" ]]; then
+        echo
+        break
+    fi
+    printf '.'
+    sleep 2
 done
