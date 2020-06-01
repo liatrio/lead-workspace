2403cae1b28106f9598fe270bcf5d057a335318b
Michael Parker <michael@parker.gg>
2020-06-01 14:54:01 -0500

    reorder script

--- wait-for-cluster.sh
+++ wait-for-cluster.sh
@@ -1,16 +1,5 @@
 #!/usr/bin/env bash
 
-printf "Waiting for Kind node to be ready"
-while : ; do
-    nodeReadyStatus=$(kubectl get node kind-control-plane -o jsonpath="{.status.conditions[?(@.type==\"Ready\")].status}" 2>/dev/null)
-    if [[ "${nodeReadyStatus}" == "True" ]]; then
-        echo
-        break
-    fi
-    printf '.'
-    sleep 2
-done
-
 printf "Waiting for CoreDNS pods to be ready"
 while : ; do
     coreDnsReadyPods=$(kubectl get deploy -n kube-system coredns -o jsonpath="{.status.readyReplicas}" 2>/dev/null)
@@ -28,6 +17,17 @@ printf "Waiting for control plane to be ready"
 while : ; do
     status=$(kubectl get pod -n kube-system kube-apiserver-kind-control-plane -o jsonpath="{.status.phase}" 2>/dev/null)
     if [[ "${status}" == "Running" ]]; then
+        echo
+        break
+    fi
+    printf '.'
+    sleep 2
+done
+
+printf "Waiting for Kubernetes cluster to be ready"
+while : ; do
+    nodeReadyStatus=$(kubectl get node kind-control-plane -o jsonpath="{.status.conditions[?(@.type==\"Ready\")].status}" 2>/dev/null)
+    if [[ "${nodeReadyStatus}" == "True" ]]; then
         echo
         break
     fi
