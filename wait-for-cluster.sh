a6d526cd859e797c2dc45a699248c090801018a6
Michael Parker <michael@parker.gg>
2020-06-01 15:05:02 -0500

    wait for more components

--- wait-for-cluster.sh
+++ wait-for-cluster.sh
@@ -1,11 +1,20 @@
 #!/usr/bin/env bash
 
-printf "Waiting for CoreDNS pods to be ready"
+printf "Waiting for CNI to be ready"
 while : ; do
-    coreDnsReadyPods=$(kubectl get deploy -n kube-system coredns -o jsonpath="{.status.readyReplicas}" 2>/dev/null)
-    coreDnsExpectedReadyPods=$(kubectl get deploy -n kube-system coredns -o jsonpath="{.status.replicas}" 2>/dev/null)
-    coreDnsReady=$(expr ${coreDnsReadyPods} / ${coreDnsExpectedReadyPods} 2>/dev/null)
-    if [[ "${coreDnsReady}" == "1" ]]; then
+    cniReady=$(kubectl get daemonset -n kube-system kindnet -o jsonpath="{.status.numberReady}" 2>/dev/null)
+    if [[ "${cniReady}" == "1" ]]; then
+        echo
+        break
+    fi
+    printf '.'
+    sleep 2
+done
+
+printf "Waiting for kube-proxy to be ready"
+while : ; do
+    kubeProxyReady=$(kubectl get daemonset -n kube-system kube-proxy -o jsonpath="{.status.numberReady}" 2>/dev/null)
+    if [[ "${kubeProxyReady}" == "1" ]]; then
         echo
         break
     fi
@@ -17,6 +26,19 @@ printf "Waiting for control plane to be ready"
 while : ; do
     status=$(kubectl get pod -n kube-system kube-apiserver-kind-control-plane -o jsonpath="{.status.phase}" 2>/dev/null)
     if [[ "${status}" == "Running" ]]; then
+        echo
+        break
+    fi
+    printf '.'
+    sleep 2
+done
+
+printf "Waiting for CoreDNS pods to be ready"
+while : ; do
+    coreDnsReadyPods=$(kubectl get deploy -n kube-system coredns -o jsonpath="{.status.readyReplicas}" 2>/dev/null)
+    coreDnsExpectedReadyPods=$(kubectl get deploy -n kube-system coredns -o jsonpath="{.status.replicas}" 2>/dev/null)
+    coreDnsReady=$(expr ${coreDnsReadyPods} / ${coreDnsExpectedReadyPods} 2>/dev/null)
+    if [[ "${coreDnsReady}" == "1" ]]; then
         echo
         break
     fi
