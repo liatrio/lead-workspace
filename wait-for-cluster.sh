37af95e6d194d8b157cbee601fdd934cc5d93595
Michael Parker <michael@parker.gg>
2020-06-01 14:18:16 -0500

    enhance the wait script

--- wait-for-cluster.sh
+++ wait-for-cluster.sh
@@ -1,10 +1,33 @@
 #!/usr/bin/env bash
 
+printf "Waiting for Kind node to be ready"
+while : ; do
+    nodeReadyStatus=$(kubectl get node kind-control-plane -o jsonpath="{.status.conditions[?(@.type==\"Ready\")].status}" 2>/dev/null)
+    if [[ "${nodeReadyStatus}" == "True" ]]; then
+        break
+    fi
+    printf '.'
+    sleep 2
+done
+
+printf "Waiting for CoreDNS pods to be ready"
+while : ; do
+    coreDnsReadyPods=$(kubectl get deploy -n kube-system coredns -o jsonpath="{.status.readyReplicas}" 2>/dev/null)
+    coreDnsExpectedReadyPods=$(kubectl get deploy -n kube-system coredns -o jsonpath="{.status.replicas}" 2>/dev/null)
+    coreDnsReady=$(expr ${coreDnsReadyPods} / ${coreDnsExpectedReadyPods})
+    if [[ "${coreDnsReady}" == "1" ]]; then
+        break
+    fi
+    printf '.'
+    sleep 2
+done
+
+printf "Waiting for control plane to be ready"
 while : ; do
     status=$(kubectl get pod -n kube-system kube-apiserver-kind-control-plane -o jsonpath="{.status.phase}" 2>/dev/null)
     if [[ "${status}" == "Running" ]]; then
         break
     fi
-    sleep 15
-    echo "Waiting for cluster to finish setting up..."
+    printf '.'
+    sleep 2
 done
